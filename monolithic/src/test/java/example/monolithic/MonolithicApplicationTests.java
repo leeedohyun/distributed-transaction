550a0048a60fe9fb17fd852de31bb1b98b75f72d
leeedohyun <ldh19738@gmail.com>
2025-10-03 20:49:57 +0900

    feat: 프로젝트 세팅

---
+++ monolithic/src/test/java/example/monolithic/MonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package example.monolithic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
